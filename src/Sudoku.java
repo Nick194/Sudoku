d86fceb2f1a40e362de090c2482a157053555f4f
Nicholas Taylor <nxt654@cs.bham.ac.uk>
2017-11-17 16:07:48 +0000

    Refactored classes Sudoku and SudokuRead

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -5,100 +5,106 @@
  * @version 2016/11/30
  */
 public class Sudoku {
-	public int[][] array;
-
-	/**
-	 * The constructor for Sudoku
-	 * @param array The sudoku array.
-	 */
-	public Sudoku(int[][] array) {
-		this.array = array;
-	}
-	/**
-	 * The getter for array.
-	 * @return The array.
-	 */
-	public int[][] getArray() {
-		return array;
-	}
-	/**
-	 * The is filled method which checks if the sudoku array has any zeros in it. If it does it returns false.
-	 * @param array The sudoku array
-	 * @return sudokuFilled The boolean which tells if the sudoku is filled or not.
-	 */
-	public static boolean isFilled(int[][] array) {
-		boolean sudokuFilled = true;
-		for (int i = 0; i < 9; i++) {
-
-			for (int j = 0; j < 9; j++) {
-				if (array[i][j] == 0) {
-					sudokuFilled = false;
-					
-				} 
-
-			}
-		}
-		if (sudokuFilled == false) {
-		System.out.println("The sudoku is not filled in!");
-		} else {
-			System.out.println("The sudoku is complete!");
-		}
-		return sudokuFilled;
-
-	}
-	
-	/**
-	 * toString method for Sudoku.
-	 * @param all The String that is iteratively added to to produce the sudoku in the correct format.
-	 * Here a nested for loop iterates over array, and adds those numbers that are not equal to zero to the string all, and those that are are represented by a blank space.
-	 */
-	public String toString() {
-		String all ="";
-		all="\n" + "++===+===+===++===+===+===++===+===+===++" + "\n";
-		for (int i = 0; i < 9; i++) {
-			all= all+"|";
-			for (int j = 0; j < 9; j++) {
-				if (array[i][j] != 0 && j != 2 && j != 5) {
-					all = all + "| " + array[i][j] + " ";
-				} else if (array[i][j] == 0) {
-					all = all + "| " + "  ";
-				} 
-				if (j == 2 && array[i][j] != 0 || j == 5 && array[i][j] != 0) {
-					all = all + "| " + array[i][j] + " |";
-				} 
-				if (j == 2 && array[i][j] == 0 || j == 5 && array[i][j] == 0) {
-					all = all + "|";
-				}
-
-			}
-			if (i == 0 || i == 1 || i == 3 || i == 4 || i == 6 || i == 7) {
-				all = all + "||" + "\n" + "++---+---+---++---+---+---++---+---+---++" + "\n";
-			} else if (i == 2 || i == 5 || i == 8) {
-				all = all + "||" + "\n" + "++===+===+===++===+===+===++===+===+===++" + "\n";
-			}
-		}
-		return all;
-	}
-
-	/**
-	 * Main method for class Sudoku.
-	 * @param args
-	 * Here a 2d array is declared called array, it has the same numbers as the example in exercise 1. This array is then 
-	 */
-	public static void main(String[] args) {
-		int array [][] =  {{1, 2, 3, 4, 5, 6, 7, 8, 9},
-				{4, 5, 6, 7, 8, 9, 1, 2, 3},
-				{7, 8, 9, 1, 2, 3, 4, 5, 6},
-				{2, 3, 4, 5, 6, 7, 8, 9, 1},
-				{5, 6, 7, 8, 9, 1, 2, 3, 4},
-				{8, 9, 1, 2, 3, 4, 5, 6, 7},
-				{3, 4, 5, 6, 7, 8, 9, 1, 2},
-				{6, 7, 8, 9, 1, 2, 3, 4, 5},
-				{9, 1, 2, 3, 4, 5, 6, 7, 8}}; 
-		Sudoku test2 = new Sudoku(array);
-		System.out.println(test2.toString());
-		System.out.print(Sudoku.isFilled(array));
-		//Sudoku.isFilled(array);
-
-	}
+    public int[][] array;
+    
+    
+    /**
+     * The constructor for Sudoku
+     * @param array The sudoku array.
+     */
+    public Sudoku(int[][] array) {
+        this.array = array;
+    }
+    
+    /**
+     * The getter for array.
+     * @return The array.
+     */
+    public int[][] getArray() {
+        return array;
+    }
+    
+    /**
+     * The is filled method which checks if the sudoku array has any zeros in it. If it does it returns false.
+     * @param array The sudoku array
+     * @return sudokuFilled The boolean which tells if the sudoku is filled or not.
+     */
+    public static boolean isFilled(int[][] array) {
+        boolean sudokuFilled = true;
+        
+        for (int i = 0; i < 9; i++) {
+            
+            for (int j = 0; j < 9; j++) {
+                if (array[i][j] == 0) {
+                    sudokuFilled = false;
+                    
+                }
+                
+            }
+        }
+        
+        return sudokuFilled;
+        
+    }
+    
+    /**
+     * toString method for Sudoku.
+     * @param all The String that is iteratively added to to produce the sudoku in the correct format.
+     * Here a nested for loop iterates over array, and adds those numbers that are not equal to zero to the string all, and those that are are represented by a blank space.
+     */
+    public String toString() {
+        String topAndBottom = "++===+===+===++===+===+===++===+===+===++";
+        String middle = "++---+---+---++---+---+---++---+---+---++";
+        String result = "";
+        
+        for (int i = 0; i < 9; i++) {
+            
+            if(i == 0 || i % 3 == 0){
+                result += "\n" + topAndBottom + "\n";
+                
+            } else {
+                result += "\n" + middle + "\n";
+            }
+            for (int j = 0; j < 9; j++) {
+                
+                if(j == 0){
+                    result += "|| " + array[i][j] + " |";
+                    
+                } else if(j % 3 == 0){
+                    result += "| " + array[i][j] + " |";
+                    
+                } else if(j == 8){
+                    result += " " + array[i][j] + " ||";
+                    
+                } else {
+                    result += " " + array[i][j] + " |";
+                }
+            }
+        }
+        result += "\n" + topAndBottom + "\n";
+        
+        return result;
+    }
+    
+    /**
+     * Main method for class Sudoku.
+     * @param args
+     * Here a 2d array is declared called array, it has the same numbers as the example in exercise 1. This array is then
+     */
+    public static void main(String[] args) {
+        int array [][] =  {{1, 2, 3, 4, 5, 6, 7, 8, 9},
+            {4, 5, 6, 7, 8, 9, 1, 2, 3},
+            {7, 8, 9, 1, 2, 3, 4, 5, 6},
+            {2, 3, 4, 5, 6, 7, 8, 9, 1},
+            {5, 6, 7, 8, 9, 1, 2, 3, 4},
+            {8, 9, 1, 2, 3, 4, 5, 6, 7},
+            {3, 4, 5, 6, 7, 8, 9, 1, 2},
+            {6, 7, 8, 9, 1, 2, 3, 4, 5},
+            {9, 1, 2, 3, 4, 5, 6, 7, 8}};
+        Sudoku test2 = new Sudoku(array);
+        System.out.println(test2.toString());
+        System.out.print(Sudoku.isFilled(array));
+        
+    }
 }
+
--- src/SudokuRead.java
+++ src/SudokuRead.java
@@ -2,91 +2,97 @@ import java.io.*;
 
 
 /**
- * SudokuRead reads in a sudoku and prints it out in the prefered toString method inherited from class Sudoku. 
+ * SudokuRead reads in a sudoku and prints it out in the prefered toString method inherited from class Sudoku.
  * @author Nicholas Taylor
  * @version 2016/11/30
  */
 public class SudokuRead extends Sudoku {
-	/**
-	 * The getter for array.
-	 */
-	@Override
-	public int[][] getArray(){
-		return array;
-	}
-	/**
-	 * The constructor for SudokuRead
-	 * @param array
-	 */
-	public SudokuRead(int[][] array){
-		super(array);
-	}
-
-
-
-	/**
-	 * The readSudoku method that reads in a sudoku from file in the from of a plain text file. It throws two exceptions which are caught and dealt with appropriately below, 
-	 * IOException and IllegalArgumentException.
-	 * 
-	 * @param file The string for the file name of the sudoku to be read in. 
-	 * @return a1 A sudoku object of type sudoku.
-	 * @throws IllegalArgumentException Thrown if the sudoku contains an illegal element i.e. not a number from 1 - 9.
-	 * @throws IOException Thrown if the file is unreachable or doesn't exist.
-	 */
-	public static Sudoku readSudoku(String file) {
-
-		int [][] bigArray = new int [9][9];
-
-		/**
-		 * Here the file is read in via buffered reader and assigned to a String called reader. reader then has all spaces within replaced with 0s. 
-		 * Afterwards a string array called readArray is initialised and reader is split by each character into each element of readArray.
-		 * Finally a second for loop iterates through and changes the strings within readArray into ints and add them to a 2d int array called bigArray. 
-		 * bigArray is then passed into the toString method and printed in the desired format. 
-		 */
-		try { 
-			BufferedReader br = new BufferedReader(new FileReader(file)); 
-			for(int i = 0; i < 9; i++){
-				String reader = br.readLine();
-				reader = reader.replaceAll(" ", "0");
-				String [] readArray;
-				readArray = reader.split("");
-				for (int j = 0; j < 9; j++) {
-					bigArray[i][j] = Integer.parseInt(readArray[j]);
-				}
-
-
-			}
-			br.close();
-		}
-
-		catch (IOException e) {
-			System.out.println("File not found");
-		}
-		catch (IllegalArgumentException e) {
-			System.out.println("The Sudoku contains a value that is not of 1-9!");
-		}
-
-
-		Sudoku a1 = new Sudoku (bigArray);
-
-
-
-		return a1;
-	}
-	/**
-	 * main method which passes the string "./src/Sudoku.txt" to the readSudoku method. This string is the file path for finding the sudoku file to be read.
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			System.out.println(readSudoku("./src/Sudoku.txt"));
-		} 
-		catch (IllegalArgumentException e) {
-
-			e.printStackTrace();
-		}
-
-	}
-
-
+    private static BufferedReader br;
+    
+    /**
+     * The getter for array.
+     */
+    @Override
+    public int[][] getArray(){
+        return array;
+    }
+    /**
+     * The constructor for SudokuRead
+     * @param array
+     */
+    public SudokuRead(int[][] array){
+        super(array);
+    }
+    
+    
+    
+    /**
+     * The readSudoku method that reads in a sudoku from file in the from of a plain text file. It throws two exceptions which are caught and dealt with appropriately below,
+     * IOException and IllegalArgumentException.
+     *
+     * @param file The string for the file name of the sudoku to be read in.
+     * @return a1 A sudoku object of type sudoku.
+     * @throws IllegalArgumentException Thrown if the sudoku contains an illegal element i.e. not a number from 1 - 9.
+     * @throws IOException Thrown if the file is unreachable or doesn't exist.
+     */
+    public static Sudoku readSudoku(String file) throws IllegalArgumentException, IOException {
+        
+        int [][] sudokuArray = new int [9][9];
+        
+        /**
+         * Here the file is read in via buffered reader and assigned to a String called reader. reader then has all spaces within replaced with 0s.
+         * Afterwards a string array called readArray is initialised and reader is split by each character into each element of readArray.
+         * Finally a second for loop iterates through and changes the strings within readArray into ints and add them to a 2d int array called sudokuArray.
+         * sudokuArray is then passed into the toString method and printed in the desired format.
+         */
+        
+        
+        
+        
+        
+        try{
+            br = new BufferedReader(new FileReader(file));
+            
+            for(int i = 0; i < 9; i++){
+                String reader = br.readLine();
+                reader = reader.replaceAll(" ", "0");
+                String [] readArray;
+                readArray = reader.split("");
+                
+                for (int j = 0; j < 9; j++) {
+                    
+                    try{
+                        sudokuArray[i][j] = Integer.parseInt(readArray[j]);
+                    } catch (NumberFormatException e){
+                        throw new IllegalArgumentException("Sudoku file contains an incompatible Character");
+                    }
+                }
+            }
+        } catch(FileNotFoundException e){
+            throw new IOException("File cannot be found.");
+        }
+        
+        Sudoku a1 = new Sudoku (sudokuArray);
+        
+        
+        
+        return a1;
+    }
+    
+    /**
+     * main method which passes the string "./src/Sudoku.txt" to the readSudoku method. This string is the file path for finding the sudoku file to be read.
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            System.out.println(readSudoku("./src/Sudoku.txt"));
+        }
+        catch (IllegalArgumentException | IOException e) {
+            
+            e.printStackTrace();
+        }
+        
+    }
+    
+    
 }
